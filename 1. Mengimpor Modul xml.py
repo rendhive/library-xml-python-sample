87f176a5bcbec2781fa077195dbe3b34a4583fd4
rendhive <rendhivecode@gmail.com>
2026-06-13 11:21:20 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul xml.py

---
+++ 1. Mengimpor Modul xml.py
@@ -0,0 +1,5 @@
+import xml.etree.ElementTree as ET
+
+print("Modul xml.etree.ElementTree berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk memproses XML menggunakan ElementTree.
+# Kondisi: Digunakan sebelum memanipulasi atau mem-parsing file XML.
